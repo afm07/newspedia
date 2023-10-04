b8489d43db214b2a6afdb67e89bca73ed52a8523
afm07 <agasfaridmuhammad@gmail.com>
2023-10-05 01:40:38 +0700

    build news List

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -4,7 +4,9 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:news_app/models/news_model.dart';
+import 'package:news_app/ui/pages/news.dart';
 import 'package:news_app/ui/widget/carousel.dart';
+import 'package:news_app/ui/widget/news_tag.dart';
 
 import 'package:provider/provider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -64,11 +66,11 @@ class _MyHomePageState extends State<MyHomePage> {
             AppHeader(themeState: themeState),
             isLoading
                 ? buildLoading()
-                : ListView(
-                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                    shrinkWrap: true,
-                    children: [
-                      Column(
+                : Expanded(
+                    flex: 1,
+                    child: SingleChildScrollView(
+                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           const SectionTitle(
@@ -80,9 +82,19 @@ class _MyHomePageState extends State<MyHomePage> {
                             height: 10,
                           ),
                           buildCarousel(),
+                          const SizedBox(height: 30),
+                          const SectionTitle(
+                            title: 'Recommendations',
+                            subtitle:
+                                'A collection of hot news recommended for you.',
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          buildNewsList(newsModel.toList()),
                         ],
                       ),
-                    ],
+                    ),
                   ),
           ],
         ),
@@ -157,4 +169,108 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
   }
+
+  Widget buildNewsList(List<NewsModel> newsModel) => ListView.builder(
+        shrinkWrap: true,
+        itemCount: newsModel.length,
+        itemBuilder: (context, index) {
+          final news = newsModel[index];
+          return InkWell(
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                '/news',
+                arguments: News(
+                  newsModel: newsModel,
+                  selected: index,
+                ),
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Image.network(
+                            news.urlToImage.toString(),
+                            fit: BoxFit.cover,
+                            height: 120.0,
+                            width: 100.0,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.only(bottom: 8),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 8,
+                                  right: 8,
+                                  top: 5,
+                                ),
+                                child: NewsTag(
+                                  tag: news.category.toString(),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 8,
+                                  right: 8,
+                                  top: 8,
+                                ),
+                                child: Text(
+                                  news.title.toString(),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 8,
+                                  right: 8,
+                                  top: 8,
+                                ),
+                                child: Text(
+                                  news.description.toString(),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Divider(
+                    height: 20,
+                    thickness: 2,
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      );
 }
